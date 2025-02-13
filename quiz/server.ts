a49164b33f8a3778c7a3914ea64e7c39e5785c22
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2025-02-13 11:18:13 -0500

    added few comments

--- quiz/server.ts
+++ quiz/server.ts
@@ -1,8 +1,11 @@
-import fs from 'fs';
+import  { promises as fsPromises } from 'fs';
 import path from 'path';
 import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
+/**
+ * A type that represents a user object
+ */
 interface User {
   id: number;
   firstName: string;
@@ -11,23 +14,38 @@ interface User {
   email: string;
 }
 
+/**
+ * A type that represents the request received by the server
+ */
 interface UserRequest extends Request {
   users?: User[];
 }
 
+// the server application object created by the express server
 const app: Express = express();
 const port: number = 8000;
 
+// path to test user data
 const dataFile = '../data/users.json';
 
 let users: User[];
 
-fs.readFile(path.resolve(__dirname, dataFile), (err, data) => {
-  console.log('reading file ... ');
-  if (err) throw err;
-  users = JSON.parse(data.toString());
-});
+// a synchronous function that reads the user data from the file
+async function readUsersFile() {
+  try {
+    console.log('reading file ... ');
+    const data = await fsPromises.readFile(path.resolve(__dirname, dataFile));
+    users = JSON.parse(data.toString());
+    console.log('File read successfully');
+  } catch (err) {
+    console.error('Error reading file:', err);
+    throw err;
+  }
+}
 
+readUsersFile();
+
+// a middleware function that adds the users data to the request object
 const addMsgToRequest = (req: UserRequest, res: Response, next: NextFunction) => {
   if (users) {
     req.users = users;
@@ -39,9 +57,12 @@ const addMsgToRequest = (req: UserRequest, res: Response, next: NextFunction) =>
   }
 };
 
+// a middleware function the verifies the origin of the request using a cors package
 app.use(cors({ origin: 'http://localhost:3000' }));
+// adds the middleware function to the application
 app.use('/read/usernames', addMsgToRequest);
 
+// a route that sends the usernames of the users to the client
 app.get('/read/usernames', (req: UserRequest, res: Response) => {
   let usernames = req.users?.map((user) => {
     return { id: user.id, username: user.username };
@@ -49,18 +70,29 @@ app.get('/read/usernames', (req: UserRequest, res: Response) => {
   res.send(usernames);
 });
 
+// a middleware function that parses the request body to json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// adds the middleware function to the application for POST requests
 app.use('/write/adduser', addMsgToRequest);
 
-app.post('/write/adduser', (req: UserRequest, res: Response) => {
-  let newuser = req.body as User;
-  users.push(newuser);
-  fs.writeFile(path.resolve(__dirname, dataFile), JSON.stringify(users), (err) => {
-    if (err) console.log('Failed to write');
-    else console.log('User Saved');
-  });
-  res.send('done');
+// a route that receives a user object and saves it to the user data file
+app.post('/write/adduser', async (req: UserRequest, res: Response) => {
+  try {
+    let newuser = req.body as User;
+    users.push(newuser);
+    
+    await fsPromises.writeFile(
+      path.resolve(__dirname, dataFile), 
+      JSON.stringify(users)
+    );
+    
+    console.log('User Saved');
+    res.send('done');
+  } catch (err) {
+    console.log('Failed to write:', err);
+    res.status(500).send('Error saving user');
+  }
 });
 
 app.listen(port, () => {
--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
+// A type that represents a user object
 interface User {
   id: number;
   firstName: string;
@@ -11,17 +12,21 @@ interface User {
   email: string;
 }
 
+// A type that represents the request received by the server
 interface UserRequest extends Request {
   users?: User[];
 }
 
+// the server application object created by the express server
 const app: Express = express();
 const port: number = 8000;
 
+// path to test user data
 const dataFile = './data/users.json';
 
 let users: User[];
 
+//  a synchronous function that reads the user data from the file
 async function readUsersFile() {
   try {
     console.log('reading file ... ');
@@ -36,6 +41,7 @@ async function readUsersFile() {
 
 readUsersFile();
 
+// a middleware function that adds the users data to the request object
 const addMsgToRequest = (req: UserRequest, res: Response, next: NextFunction) => {
   if (users) {
     req.users = users;
@@ -47,9 +53,12 @@ const addMsgToRequest = (req: UserRequest, res: Response, next: NextFunction) =>
   }
 };
 
+// a middleware function the verifies the origin of the request using a cors package
 app.use(cors({ origin: 'http://localhost:3000' }));
+// adds the middleware function to the GET route
 app.use('/read/usernames', addMsgToRequest);
 
+// GET route that returns a list of usernames
 app.get('/read/usernames', (req: UserRequest, res: Response) => {
   let usernames = req.users?.map((user) => {
     return { id: user.id, username: user.username };
@@ -57,10 +66,13 @@ app.get('/read/usernames', (req: UserRequest, res: Response) => {
   res.send(usernames);
 });
 
+// middleware function that parses the request body to json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// adds the middleware function to the POST route
 app.use('/write/adduser', addMsgToRequest);
 
+// POST route that adds a new user to the list
 app.post('/write/adduser', async (req: UserRequest, res: Response) => {
   try {
     let newuser = req.body as User;
